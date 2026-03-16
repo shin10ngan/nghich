3f77d2069e0aebcdac10dd884235844173d895be
ChiBa <quanafk10@gmail.com>
2026-03-17 03:09:34 +0700

    content

--- src/App.js
+++ src/App.js
@@ -4,20 +4,8 @@ import './App.css';
 function App() {
 return (
 <div class="bg bg-info">
-  <table class="border+border={2}">
-    <tr>
-      <th>stt</th>
-      <th>msv</th>
-      <th>ten</th>
-      <th>diem</th>
-    </tr>
-    <tr>
-      <th>1</th>
-      <th>465</th>
-      <th>quan</th>
-      <th>10</th>
-    </tr>
-  </table>
+
+    có con cật node
 </div>
 );
 }
