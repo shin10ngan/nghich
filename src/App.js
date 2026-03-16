0b0cbb102f91e3ea325b38148e036180708ee103
ChiBa <quanafk10@gmail.com>
2026-03-17 02:27:53 +0700

    table error

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+return (
+<div>
+  <table class="border={2}">
+    <tr>
+      <th>stt</th>
+      <th>msv</th>
+      <th>ten</th>
+      <th>diem</th>
+    </tr>
+    <tr>
+      <th>1</th>
+      <th>465</th>
+      <th>quan</th>
+      <th>10</th>
+    </tr>
+  </table>
+</div>
+);
+}
+
+export default App;
